f429029a2a87015b66270ab3b7cc8aa4527af6cd
Kyojur0 <crimson.995890@gmail.com>
2025-02-03 00:50:53 +0000

    bruuh plzz

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def test_write():
             db.session.commit()
         return {"message": "Write operation successful"}, 200
     except Exception as e:
-
+        return {"error": str(e)}, 500
+    
 if __name__ == '__main__':
     app.run(debug=True)
